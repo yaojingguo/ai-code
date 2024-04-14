9f1ba6d7f17b62ec4397ce07e75bfd0eed392d49
Jingguo Yao <yaojingguo@gmail.com>
2024-04-14 14:02:44 +0800

    First commit

---
+++ acc.py
@@ -0,0 +1,80 @@
+from datasets import load_dataset
+from transformers import AutoTokenizer, DataCollatorWithPadding
+from accelerate import Accelerator
+from transformers import AdamW, AutoModelForSequenceClassification, get_scheduler
+from torch.utils.data import DataLoader
+from tqdm.auto import tqdm
+import evaluate
+import torch
+
+def training_function():
+    raw_datasets = load_dataset("glue", "mrpc")
+    checkpoint = "bert-base-uncased"
+    tokenizer = AutoTokenizer.from_pretrained(checkpoint)
+
+    def tokenize_function(example):
+        return tokenizer(example["sentence1"], example["sentence2"], truncation=True)
+
+    tokenized_datasets = raw_datasets.map(tokenize_function, batched=True)
+    data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
+
+    tokenized_datasets = tokenized_datasets.remove_columns(["sentence1", "sentence2", "idx"])
+    tokenized_datasets = tokenized_datasets.rename_column("label", "labels")
+    tokenized_datasets.set_format("torch")
+    print("column names:", tokenized_datasets["train"].column_names)
+
+    train_dataloader = DataLoader(
+        tokenized_datasets["train"], shuffle=True, batch_size=8, collate_fn=data_collator
+    )
+    eval_dataloader = DataLoader(
+        tokenized_datasets["validation"], batch_size=8, collate_fn=data_collator
+    )
+
+    accelerator = Accelerator()
+
+    model = AutoModelForSequenceClassification.from_pretrained(checkpoint, num_labels=2)
+    optimizer = AdamW(model.parameters(), lr=3e-5)
+
+    train_dl, eval_dl, model, optimizer = accelerator.prepare(
+        train_dataloader, eval_dataloader, model, optimizer
+    )
+
+    num_epochs = 3
+    num_training_steps = num_epochs * len(train_dl)
+    lr_scheduler = get_scheduler(
+        "linear",
+        optimizer=optimizer,
+        num_warmup_steps=0,
+        num_training_steps=num_training_steps,
+    )
+
+    progress_bar = tqdm(range(num_training_steps))
+
+    model.train()
+    for epoch in range(num_epochs):
+        for batch in train_dl:
+            outputs = model(**batch)
+            loss = outputs.loss
+            accelerator.backward(loss)
+
+            optimizer.step()
+            lr_scheduler.step()
+            optimizer.zero_grad()
+            progress_bar.update(1)
+
+
+    metric = evaluate.load("glue", "mrpc")
+    device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
+    model.eval()
+    for batch in eval_dataloader:
+        batch = {k: v.to(device) for k, v in batch.items()}
+        with torch.no_grad():
+            outputs = model(**batch)
+
+        logits = outputs.logits
+        predictions = torch.argmax(logits, dim=-1)
+        metric.add_batch(predictions=predictions, references=batch["labels"])
+
+    print("metric:", metric.compute())
+
+training_function()
