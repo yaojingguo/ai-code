788bcc3f2ecb744fc04bf1da575b615997b54063
Jingguo Yao <yaojingguo@gmail.com>
2024-05-05 17:04:24 +0800

    update

--- chapter-7/code.py
+++ chapter-7/code.py
@@ -1,37 +1,28 @@
-from transformers import AutoTokenizer
-import collections
 import numpy as np
-from transformers import default_data_collator
+from transformers import AutoTokenizer
+import evaluate
 
-tokenizer = AutoTokenizer.from_pretrained("")
+metric = evaluate.load("sacrebleu")
 
-wwm_probability = 0.2
+model_checkpoint = "Helsinki-NLP/opus-mt-en-fr"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, return_tensors="pt")
 
-def whole_word_masking_data_collator(features):
-    for feature in features:
-        word_ids = feature.pop("word_ids")
 
-        # Create a map between words and corresponding token indices
-        mapping = collections.defaultdict(list)
-        current_word_index = -1
-        current_word = None
-        for idx, word_id in enumerate(word_ids):
-            if word_id is not None:
-                if word_id != current_word:
-                    current_word = word_id
-                    current_word_index += 1
-                mapping[current_word_index].append(idx)
+def compute_metrics(eval_preds):
+    preds, labels = eval_preds
+    # In case the model returns more than the prediction logits
+    if isinstance(preds, tuple):
+        preds = preds[0]
 
-        # Randomly mask words
-        mask = np.random.binomial(1, wwm_probability, (len(mapping),))
-        input_ids = feature["input_ids"]
-        labels = feature["labels"]
-        new_labels = [-100] * len(labels)
-        for word_id in np.where(mask)[0]:
-            word_id = word_id.item()
-            for idx in mapping[word_id]:
-                new_labels[idx] = labels[idx]
-                input_ids[idx] = tokenizer.mask_token_id
-        feature["labels"] = new_labels
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
 
-    return default_data_collator(features)
+    # Replace -100s in the labels as we can't decode them
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    # Some simple post-processing
+    decoded_preds = [pred.strip() for pred in decoded_preds]
+    decoded_labels = [[label.strip()] for label in decoded_labels]
+
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
+    return {"bleu": result["score"]}
