111d2af6599ecc76f02c219ac5f08dcfd12b1845
Jingguo Yao <yaojingguo@gmail.com>
2024-05-12 19:34:24 +0800

    update

--- chapter-7/code.py
+++ chapter-7/code.py
@@ -1,28 +1,27 @@
-import numpy as np
 from transformers import AutoTokenizer
+import numpy as np
+from nltk.tokenize import sent_tokenize
 import evaluate
 
-metric = evaluate.load("sacrebleu")
+model_checkpoint = "google/mt5-small"
+tokenizer = AutoTokenizer.from_pretrained(model_checkpoint)
+rouge_score = evaluate.load("rouge")
 
-model_checkpoint = "Helsinki-NLP/opus-mt-en-fr"
-tokenizer = AutoTokenizer.from_pretrained(model_checkpoint, return_tensors="pt")
-
-
-def compute_metrics(eval_preds):
-    preds, labels = eval_preds
-    # In case the model returns more than the prediction logits
-    if isinstance(preds, tuple):
-        preds = preds[0]
-
-    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
-
-    # Replace -100s in the labels as we can't decode them
+def compute_metrics(eval_pred):
+    predictions, labels = eval_pred
+    # Decode generated summaries into text
+    decoded_preds = tokenizer.batch_decode(predictions, skip_special_tokens=True)
+    # Replace -100 in the labels as we can't decode them
     labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    # Decode reference summaries into text
     decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
-
-    # Some simple post-processing
-    decoded_preds = [pred.strip() for pred in decoded_preds]
-    decoded_labels = [[label.strip()] for label in decoded_labels]
-
-    result = metric.compute(predictions=decoded_preds, references=decoded_labels)
-    return {"bleu": result["score"]}
+    # ROUGE expects a newline after each sentence
+    decoded_preds = ["\n".join(sent_tokenize(pred.strip())) for pred in decoded_preds]
+    decoded_labels = ["\n".join(sent_tokenize(label.strip())) for label in decoded_labels]
+    # Compute ROUGE scores
+    result = rouge_score.compute(
+        predictions=decoded_preds, references=decoded_labels, use_stemmer=True
+    )
+    # Extract the median scores
+    result = {key: value * 100 for key, value in result.items()}
+    return {k: round(v, 4) for k, v in result.items()}
